28aa1e070a8a6d79344cbac0c2bd4f705b570a44
SUB-ZERO <116029937+salman11xi@users.noreply.github.com>
2023-09-16 10:43:35 -0700

    Tools

---
+++ tools.py
@@ -0,0 +1,31 @@
+import subprocess
+import pyfiglet
+name = pyfiglet.figlet_format("salman")
+print(name)
+print("enter you ip")
+i=input()
+subprocess.call("ping "+i,shell=True)
+
+
+# import subprocess
+# import pyfiglet
+# name = pyfiglet.figlet_format("SUB __ZERO")
+# print(name)
+
+# print("Enter ip or Domain name")
+# i = input()
+# subprocess.call("ping"+i,shell=True)
+
+
+
+# import subprocess
+# import pyfiglet
+# name = pyfiglet.figlet_format("SUB-ZERO")
+# print(name)
+
+# print("Enter ip or Domain name")
+# i =input()
+# subprocess.call("ping "+i,shell=True)
+
+
+
